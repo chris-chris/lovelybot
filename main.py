f822b1d88807cc349c738f9b20c298a1d74f1299
Chris <sjhshy@gmail.com>
2017-07-02 23:54:24 +0900

    port

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from microsoftbotframework import MsBot
 from tasks import echo_response
+import os
 
-bot = MsBot(port=80)
+bot = MsBot(port=int(os.environ['PORT']))
 bot.add_process(echo_response)
 bot.run()
